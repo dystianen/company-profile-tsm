259c88a7ab48142d2efb988eea00c5ea0325cb62
Dystian En <distian.enyus@gmail.com>
2024-12-03 10:54:27 +0700

    feat: recaptcha in contact us

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,5 +1,8 @@
+"use client";
+
 import config from "@config/config.json";
 import { markdownify } from "@lib/utils/textConverter";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 const Contact = ({ data }) => {
   const { frontmatter } = data;
@@ -7,69 +10,81 @@ const Contact = ({ data }) => {
   const { contact_form_action } = config.params;
 
   return (
-    <section className="section">
-      <div className="container">
-        {markdownify(title, "h1", "text-center font-normal")}
-        <div className="section row pb-0">
-          <div className="col-12 md:col-6 lg:col-7">
-            <form
-              className="contact-form"
-              method="POST"
-              action={contact_form_action}
-            >
-              <div className="mb-3">
-                <input
-                  className="form-input w-full rounded"
-                  name="name"
-                  type="text"
-                  placeholder="Name"
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <input
-                  className="form-input w-full rounded"
-                  name="email"
-                  type="email"
-                  placeholder="Your email"
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <input
-                  className="form-input w-full rounded"
-                  name="subject"
-                  type="text"
-                  placeholder="Subject"
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <textarea
-                  className="form-textarea w-full rounded-md"
-                  rows="7"
-                  placeholder="Your message"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Send Now
-              </button>
-            </form>
-          </div>
-          <div className="content col-12 md:col-6 lg:col-5">
-            {markdownify(info.title, "h4")}
-            {markdownify(info.description, "p", "mt-4")}
-            <ul className="contact-list mt-5">
-              {info.contacts.map((contact, index) => (
-                <li key={index}>
-                  {markdownify(contact, "strong", "text-dark")}
-                </li>
-              ))}
-            </ul>
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? ""}
+    >
+      <section className="section">
+        <div className="container">
+          {markdownify(title, "h1", "text-center font-normal")}
+          <div className="section row pb-0">
+            <div className="col-12 md:col-6 lg:col-7">
+              <form
+                className="contact-form"
+                method="POST"
+                action={contact_form_action}
+              >
+                <div className="mb-3">
+                  <input
+                    className="form-input w-full rounded"
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    className="form-input w-full rounded"
+                    name="email"
+                    type="email"
+                    placeholder="Your email"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    className="form-input w-full rounded"
+                    name="phone_number"
+                    type="number"
+                    placeholder="Phone Number"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    className="form-input w-full rounded"
+                    name="company_name"
+                    placeholder="Company Name"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <textarea
+                    className="form-textarea w-full rounded-md"
+                    rows="7"
+                    placeholder="Your message"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Send Now
+                </button>
+              </form>
+            </div>
+            <div className="content col-12 md:col-6 lg:col-5">
+              {markdownify(info.title, "h4")}
+              {markdownify(info.description, "p", "mt-4")}
+              <ul className="contact-list mt-5">
+                {info.contacts.map((contact, index) => (
+                  <li key={index}>
+                    {markdownify(contact, "strong", "text-dark")}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </GoogleReCaptchaProvider>
   );
 };
 
